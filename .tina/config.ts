5d9162d606b3cbfef96d0303f58e807673f84c26
pierre. chrzanowski <pierre.chrzanowski@gmail.com>
2023-01-16 19:48:43 -0500

    Added TinaCMS

---
+++ .tina/config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "ecb3de53-de70-4707-876b-13e541eaf536", // Get this from tina.io
+  token: null, // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "/",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "assets",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        label: "Posts",
+        name: "posts",
+        path: "_posts",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
